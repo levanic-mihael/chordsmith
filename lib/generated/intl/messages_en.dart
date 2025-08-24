e526ad9d0dd3771b836b2d37ba4c2c1a96f18c3d
levanic_mihael <xoskeletghost@gmail.com>
2025-08-24 22:43:05 +0200

    Localization files

---
+++ lib/generated/intl/messages_en.dart
@@ -0,0 +1,111 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  static String m0(chordName) =>
+      "Are you sure you want to delete \"${chordName}\"?";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+    "account": MessageLookupByLibrary.simpleMessage("Account"),
+    "alt": MessageLookupByLibrary.simpleMessage("Alt"),
+    "alternative": MessageLookupByLibrary.simpleMessage("Alternative"),
+    "alternativeChordSaved": MessageLookupByLibrary.simpleMessage(
+      "Alternative chord saved.",
+    ),
+    "alternativeChords": MessageLookupByLibrary.simpleMessage(
+      "Alternative Chords",
+    ),
+    "appTitle": MessageLookupByLibrary.simpleMessage("Chordsmith"),
+    "cancel": MessageLookupByLibrary.simpleMessage("Cancel"),
+    "chord": MessageLookupByLibrary.simpleMessage("Chord"),
+    "chordSaved": MessageLookupByLibrary.simpleMessage("Chord saved."),
+    "confirmDeleteChord": m0,
+    "create": MessageLookupByLibrary.simpleMessage("Create"),
+    "createChord": MessageLookupByLibrary.simpleMessage("Create Chord"),
+    "custom": MessageLookupByLibrary.simpleMessage("Custom"),
+    "customChord": MessageLookupByLibrary.simpleMessage("Custom Chord"),
+    "customChordSaved": MessageLookupByLibrary.simpleMessage(
+      "Custom chord saved.",
+    ),
+    "customChords": MessageLookupByLibrary.simpleMessage("Custom Chords"),
+    "darkMode": MessageLookupByLibrary.simpleMessage("Dark Mode"),
+    "deleteChord": MessageLookupByLibrary.simpleMessage("Delete Chord"),
+    "edit": MessageLookupByLibrary.simpleMessage("Edit"),
+    "editAlternativeChord": MessageLookupByLibrary.simpleMessage(
+      "Edit Alternative Chord",
+    ),
+    "editChord": MessageLookupByLibrary.simpleMessage("Edit Chord"),
+    "editChords": MessageLookupByLibrary.simpleMessage("Edit Chords"),
+    "editCustomChord": MessageLookupByLibrary.simpleMessage(
+      "Edit Custom Chord",
+    ),
+    "enterChordNameHint": MessageLookupByLibrary.simpleMessage("Chord name"),
+    "enterCustomChordName": MessageLookupByLibrary.simpleMessage(
+      "Enter custom chord name",
+    ),
+    "enterNameForNewChord": MessageLookupByLibrary.simpleMessage(
+      "Enter name for new chord:",
+    ),
+    "favorites": MessageLookupByLibrary.simpleMessage("Favorites"),
+    "markFavorite": MessageLookupByLibrary.simpleMessage("Mark as Favorite"),
+    "newChord": MessageLookupByLibrary.simpleMessage("New Chord"),
+    "no": MessageLookupByLibrary.simpleMessage("No"),
+    "noCustomChordsYet": MessageLookupByLibrary.simpleMessage(
+      "No custom chords yet",
+    ),
+    "noFavoritesYet": MessageLookupByLibrary.simpleMessage("No favorites yet"),
+    "noSuchChordFound": MessageLookupByLibrary.simpleMessage(
+      "No such chord found",
+    ),
+    "pleaseSetChordFingers": MessageLookupByLibrary.simpleMessage(
+      "Please set chord fingers on fretboard",
+    ),
+    "reports": MessageLookupByLibrary.simpleMessage("Reports"),
+    "saveChord": MessageLookupByLibrary.simpleMessage("Save Chord"),
+    "saveSettings": MessageLookupByLibrary.simpleMessage("Save Settings"),
+    "search": MessageLookupByLibrary.simpleMessage("Search"),
+    "selectChordToCreateAlternative": MessageLookupByLibrary.simpleMessage(
+      "Select chord to create alternative for:",
+    ),
+    "selectChordType": MessageLookupByLibrary.simpleMessage(
+      "Select Chord Type:",
+    ),
+    "selectMode": MessageLookupByLibrary.simpleMessage("Select Mode:"),
+    "selectTone": MessageLookupByLibrary.simpleMessage("Select Tone:"),
+    "selectToneModeType": MessageLookupByLibrary.simpleMessage(
+      "Select tone, mode, and type.",
+    ),
+    "settings": MessageLookupByLibrary.simpleMessage("Settings"),
+    "settingsSaved": MessageLookupByLibrary.simpleMessage("Settings saved"),
+    "share": MessageLookupByLibrary.simpleMessage("Share"),
+    "showChord": MessageLookupByLibrary.simpleMessage("Show Chord"),
+    "standard": MessageLookupByLibrary.simpleMessage("Standard"),
+    "standardChordNotFound": MessageLookupByLibrary.simpleMessage(
+      "Standard chord not found.",
+    ),
+    "standardChords": MessageLookupByLibrary.simpleMessage("Standard Chords"),
+    "thisChord": MessageLookupByLibrary.simpleMessage("this chord"),
+    "unknown": MessageLookupByLibrary.simpleMessage("Unknown"),
+    "unmarkFavorite": MessageLookupByLibrary.simpleMessage("Unmark Favorite"),
+    "yes": MessageLookupByLibrary.simpleMessage("Yes"),
+  };
+}
---
+++ lib/generated/intl/messages_hr.dart
@@ -0,0 +1,115 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a hr locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'hr';
+
+  static String m0(chordName) =>
+      "Jeste li sigurni da želite izbrisati \"${chordName}\"?";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+    "account": MessageLookupByLibrary.simpleMessage("Korisnički račun"),
+    "alt": MessageLookupByLibrary.simpleMessage("Alternativa"),
+    "alternative": MessageLookupByLibrary.simpleMessage("Alternativa"),
+    "alternativeChordSaved": MessageLookupByLibrary.simpleMessage(
+      "Alternativni akord je spremljen.",
+    ),
+    "alternativeChords": MessageLookupByLibrary.simpleMessage(
+      "Alternativni akordi",
+    ),
+    "appTitle": MessageLookupByLibrary.simpleMessage("Chordsmith"),
+    "cancel": MessageLookupByLibrary.simpleMessage("Odustani"),
+    "chord": MessageLookupByLibrary.simpleMessage("Akord"),
+    "chordSaved": MessageLookupByLibrary.simpleMessage("Akord je spremljen."),
+    "confirmDeleteChord": m0,
+    "create": MessageLookupByLibrary.simpleMessage("Izradi"),
+    "createChord": MessageLookupByLibrary.simpleMessage("Izradi akord"),
+    "custom": MessageLookupByLibrary.simpleMessage("Prilagođeno"),
+    "customChord": MessageLookupByLibrary.simpleMessage("Prilagođeni akord"),
+    "customChordSaved": MessageLookupByLibrary.simpleMessage(
+      "Prilagođeni akord je spremljen.",
+    ),
+    "customChords": MessageLookupByLibrary.simpleMessage("Prilagođeni akordi"),
+    "darkMode": MessageLookupByLibrary.simpleMessage("Tamni način"),
+    "deleteChord": MessageLookupByLibrary.simpleMessage("Izbriši akord"),
+    "edit": MessageLookupByLibrary.simpleMessage("Uredi"),
+    "editAlternativeChord": MessageLookupByLibrary.simpleMessage(
+      "Uredi alternativni akord",
+    ),
+    "editChord": MessageLookupByLibrary.simpleMessage("Uredi akord"),
+    "editChords": MessageLookupByLibrary.simpleMessage("Uredi akorde"),
+    "editCustomChord": MessageLookupByLibrary.simpleMessage(
+      "Uredi prilagođeni akord",
+    ),
+    "enterChordNameHint": MessageLookupByLibrary.simpleMessage("Naziv akorda"),
+    "enterCustomChordName": MessageLookupByLibrary.simpleMessage(
+      "Unesite naziv prilagođenog akorda",
+    ),
+    "enterNameForNewChord": MessageLookupByLibrary.simpleMessage(
+      "Unesite naziv novog akorda:",
+    ),
+    "favorites": MessageLookupByLibrary.simpleMessage("Favoriti"),
+    "markFavorite": MessageLookupByLibrary.simpleMessage("Označi kao favorit"),
+    "newChord": MessageLookupByLibrary.simpleMessage("Novi akord"),
+    "no": MessageLookupByLibrary.simpleMessage("Ne"),
+    "noCustomChordsYet": MessageLookupByLibrary.simpleMessage(
+      "Još nema prilagođenih akorda",
+    ),
+    "noFavoritesYet": MessageLookupByLibrary.simpleMessage("Još nema favorita"),
+    "noSuchChordFound": MessageLookupByLibrary.simpleMessage(
+      "Takav akord nije pronađen",
+    ),
+    "pleaseSetChordFingers": MessageLookupByLibrary.simpleMessage(
+      "Molimo postavite prste na pragove",
+    ),
+    "reports": MessageLookupByLibrary.simpleMessage("Izvještaji"),
+    "saveChord": MessageLookupByLibrary.simpleMessage("Spremi akord"),
+    "saveSettings": MessageLookupByLibrary.simpleMessage("Spremi postavke"),
+    "search": MessageLookupByLibrary.simpleMessage("Pretraži"),
+    "selectChordToCreateAlternative": MessageLookupByLibrary.simpleMessage(
+      "Odaberite akord za izradu alternative:",
+    ),
+    "selectChordType": MessageLookupByLibrary.simpleMessage(
+      "Odaberite tip akorda:",
+    ),
+    "selectMode": MessageLookupByLibrary.simpleMessage("Odaberite mod:"),
+    "selectTone": MessageLookupByLibrary.simpleMessage("Odaberite ton:"),
+    "selectToneModeType": MessageLookupByLibrary.simpleMessage(
+      "Odaberite ton, mod i tip.",
+    ),
+    "settings": MessageLookupByLibrary.simpleMessage("Postavke"),
+    "settingsSaved": MessageLookupByLibrary.simpleMessage(
+      "Postavke su spremljene",
+    ),
+    "share": MessageLookupByLibrary.simpleMessage("Dijeli"),
+    "showChord": MessageLookupByLibrary.simpleMessage("Prikaži akord"),
+    "standard": MessageLookupByLibrary.simpleMessage("Standardno"),
+    "standardChordNotFound": MessageLookupByLibrary.simpleMessage(
+      "Standardni akord nije pronađen.",
+    ),
+    "standardChords": MessageLookupByLibrary.simpleMessage("Standardni akordi"),
+    "thisChord": MessageLookupByLibrary.simpleMessage("ovaj akord"),
+    "unknown": MessageLookupByLibrary.simpleMessage("Nepoznato"),
+    "unmarkFavorite": MessageLookupByLibrary.simpleMessage(
+      "Ukloni iz favorita",
+    ),
+    "yes": MessageLookupByLibrary.simpleMessage("Da"),
+  };
+}
